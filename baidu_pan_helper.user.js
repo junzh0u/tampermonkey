1ebd347e3b0bc762a9205123b80c24c4718ff21c
JunZ <github@junz.info>
2018-02-23 17:44:50 -0800

    Update baidu_pan_helper.user.js

--- baidu_pan_helper.user.js
+++ baidu_pan_helper.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Auto fill the password on URLs like https://pan.baidu.com/s/xxxxxx#PSWD
 // @author       Jun Zhou
-// @match        https://pan.baidu.com/s/*
+// @match        https://pan.baidu.com/*
 // ==/UserScript==
 
 (function() {
