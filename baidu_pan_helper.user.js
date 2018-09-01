1cedaacc61dd69a470a5fb0783e0abebb78d6f43
JunZ <github@junz.info>
2018-09-01 13:28:27 -0700

    Update baidu_pan_helper.user.js

--- baidu_pan_helper.user.js
+++ baidu_pan_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baidu Pan Helper
 // @namespace    https://github.com/junzh0u/tampermonkey/
-// @version      0.1.2
+// @version      0.1.3
 // @description  Automations for BaiduPan:
 //               1. On URL like https://pan.baidu.com/s/xxxxxx#PSWD, auto fill password and submit.
 //               2. Then auto save all files (to root path).
@@ -9,6 +9,19 @@
 // @match        https://pan.baidu.com/*
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
+
+var clickSave = function() {
+    console.log('click save')
+    $('span:contains("保存到网盘"):first').click()
+    setTimeout(function() {
+        if ($('span:contains("确定")').length) {
+            console.log('confirm save')
+            $('span:contains("确定"):first').click()
+        } else {
+            clickSave()
+        }
+    }, 1000)
+}
 
 $(document).ready(function() {
     if ($('dl.pickpw').length && window.location.hash) {
@@ -19,13 +32,6 @@ $(document).ready(function() {
     } else if ($('span:contains("保存到网盘")').length) {
         console.log('select all')
         $('span:contains("全选"):first').click()
-        setTimeout(function() {
-            console.log('click save')
-            $('span:contains("保存到网盘"):first').click()
-            setTimeout(function() {
-                console.log('confirm')
-                $('span:contains("确定"):first').click()
-            }, 1000)
-        }, 1000)
+        setTimeout(clickSave, 1000)
     }
 });
